55877dbb9d19506d34ba1395e7f140e147c56484
AlbertMosella <mosella.albert@gmail.com>
2022-04-21 21:03:14 +0200

    Test to Serie Component

--- public/js/components/AppComponent.test.js
+++ public/js/components/AppComponent.test.js
@@ -3,19 +3,9 @@ import AppComponent from "./AppComponent";
 
 describe("Given a AppComponent component", () => {
   describe("When it's instantiated", () => {
-    test("Then it should create a div container", () => {
-      const container = document.createElement("div");
-      new AppComponent(container, "div", "container");
-
-      const result = container.querySelector("div");
-
-      expect(result).not.toBeNull();
-    });
-  });
-  describe("When it's instantiated", () => {
     test("Then it should render and h1 element with the text 'My Series' inside", () => {
       const container = document.createElement("div");
-      new AppComponent(container, "div", "container");
+      new AppComponent(container);
 
       const result = container.querySelector("h1").textContent;
       const expectedResult = "My Series";
